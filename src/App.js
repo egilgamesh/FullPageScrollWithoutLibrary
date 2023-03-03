9dc1609b0fb6772dce6f43367e4511ef49a4f883
egilgamesh <egilgamesh@outlook.com>
2023-03-03 21:48:44 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef } from "react";
+import Section from "./Section";
+import "./Home.css";
+import "./footer.css"
+import "./header.css"
+import Header from "./header";;
 
 function App() {
+  const section1 = useRef();
+  const section2 = useRef();
+  const section3 = useRef();
+  const section4 = useRef();
+  const section5 = useRef();
+  function scrollTo(section) {
+    section.current.scrollIntoView({ behavior: "smooth" });
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className={`container`}>
+      <Header className="header"  goToSectionRef={section1}
+            scrollTo={scrollTo}
+            showArrow={true}>
+        </Header>
+        <div ref={section1}>
+          <Section
+            image={`https://towersgame.net/img/Screenshots/IonPreview.jpg`}
+            headline={`Play Different Heroes with Different Skills And Strategies`}
+            goToSectionRef={section2}
+            scrollTo={scrollTo}
+            showArrow={true}
+          />
+        </div>
+        <div ref={section2}>
+          <Section
+            image={`https://towersgame.net/img/Screenshots/GrantPreview.jpg`}
+            headline={`Play Different Heroes with Different Skills And Strategies`}
+            goToSectionRef={section3}
+            scrollTo={scrollTo}
+            showArrow={true}
+          />
+        </div>
+
+        <div ref={section3}>
+          <Section
+            image={"https://towersgame.net/img/Screenshots/MultiplayerPreviewFarm.jpg"}
+            headline={`Play Different Heroes with Different Skills And Strategies`}
+            goToSectionRef={section5}
+            scrollTo={scrollTo}
+            showArrow={true}
+          />
+        </div>
+        <footer className="footer" ref={section5}>
+          </footer>
+      </div>
     </div>
   );
 }
---
+++ src/Section.js
@@ -0,0 +1,59 @@
+import { useEffect, useRef } from "react";
+import "./section.css";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
+
+export default function Section({
+  image,
+  headline,
+  scrollTo,
+  goToSectionRef,
+  showArrow,
+  conents,
+}) {
+  const headlineRef = useRef();
+  const sectionRef = useRef();
+  useEffect(() => {
+    gsap.fromTo(
+      headlineRef.current,
+      {
+        autoAlpha: 0,
+        y: -40,
+      },
+      {
+        y: 0,
+        autoAlpha: 1,
+        duration: 1,
+        ease: "power3.out",
+        scrollTrigger: {
+          scroller: ".container",
+          trigger: headlineRef.current,
+          start: "top 80%",
+          end: "bottom 0%",
+          toggleActions: "play none restart reverse",
+        },
+      }
+    );
+    return () => {};
+  }, []);
+  return (
+    <div className='section' ref={sectionRef}>
+
+      <div className="imageContainer">
+      <img src={image} layout={`fill`} alt="ddd"/>
+
+      </div>
+      <div className='content'>
+        <h2 ref={headlineRef}>{headline}</h2>
+      </div>
+      <p>{conents}</p>
+      {showArrow && (
+        <button
+          className="downarrow"
+          onClick={() => scrollTo(goToSectionRef)}
+        ></button>
+      )}
+    </div>
+  );
+}
